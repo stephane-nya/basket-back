de3246253a121a3a4b36b5d6cc7ee63a3de0732c
stephane <n.stephane@ymail.com>
2023-05-31 13:54:08 +0200

    modif validators 31/05/23

--- src/validators.js
+++ src/validators.js
@@ -6,7 +6,7 @@ export const validateEmail = yup.string().email().trim().label("E-mail")
 export const validatePassword = yup
   .string()
   .min(8)
-  .matches(/\w/, "Password must contain at least a special character")
+  .matches(/\W/, "Password must contain at least a special character")
   .label("Password")
 
 export const validateUsername = yup
@@ -15,7 +15,7 @@ export const validateUsername = yup
   .max(15)
   .matches(
     /^[a-z][a-z0-9._]*/,
-    "Username must contain only letters, numbers, '.' and '_' "
+    "Username must contain only letters, numbers, '.' and '_'"
   )
   .trim()
   .label("Username")
@@ -26,9 +26,7 @@ export const validateDisplayName = yup
   .max(20)
   .trim()
   .matches(/[^\n\r\u00a0]/)
-  .label("DisplayName")
-
-export const validatePage = yup.number().min(0).integer()
+  .label("Display Name")
 
 export const validateLimit = yup
   .number()
@@ -36,7 +34,31 @@ export const validateLimit = yup
   .max(config.view.results.maxLimit)
   .integer()
   .default(config.view.results.defaultLimit)
+  .label("Pagination limit")
 
-export const validateOffset = yup.number().min(0).integer().default(0)
+export const validateOffset = yup
+  .number()
+  .min(0)
+  .integer()
+  .default(0)
+  .label("Pagination offset")
 
 export const validateId = yup.number().integer().min(1).label("User ID")
+
+export const validateEmailOrUsername = yup
+  .string()
+  .min(2)
+  .trim()
+  .label("Email or Username")
+
+// export const validateContent = yup.string().min(1).label("Content")
+
+// export const validatePostTitle = yup.string().min(1).label("Title")
+
+// export const validatePostContent = validateContent.label("Post content")
+
+// export const validatePublishedAt = yup.date().label("Publishing date")
+
+// export const validateSearch = yup.string().min(3).label("Search terms")
+
+// export const validateCommentContent = validateContent.label("Comment content")
